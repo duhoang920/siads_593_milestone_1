7343735bf84ce740fb6b6a3602e57dfcbd3837fd
duhoang920 <duhoang@umich.edu>
2026-01-27 23:57:10 -0500

    Added comments
    added comments to the save_df_to_csv() function.

--- lib/data_loader.py
+++ lib/data_loader.py
@@ -20,5 +20,21 @@ def load_csv(file):
     return pd.read_csv(file)
 
 def save_df_to_csv(df, file_path):
+    """
+        Converts the DataFrames to a csv file.
+    
+        Parameters
+        ----------
+        pandas.DataFrame
+            A DataFrame containing the data.
+            
+    
+        Returns
+        -------
+        file : str or file-like object
+            Path to the CSV file or a file-like object.
+            
+    """
+    # The encoding is needed for excel csv. Without it, symbols are added into certain values.
     df.to_csv(file_path, index=False, encoding='utf-8-sig')
     print(f"Dataframe: {df} has been saved to {file_path}")
