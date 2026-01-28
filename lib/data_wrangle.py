1353d5f628f24fdb1d4a1e9d55edfb4bf6dd47fd
duhoang920 <duhoang@umich.edu>
2026-01-27 22:17:18 -0500

    Adding Census Datasets other Years
    Adding census datasets for other years. Only if needed will still focus on 2022.

--- lib/data_wrangle.py
+++ lib/data_wrangle.py
@@ -16,6 +16,39 @@ def remove_cols(df):
         pandas.DataFrame
             A DataFrame containing non-empty columns.
     """
-    df_drop_cols = df.dropna(axis=1, how='all')
+    df_drop_cols = df.dropna(how='all')
     return df_drop_cols
+
+def remove_rows(df):
+    """
+        Removes all empty rows in a DataFrame.
     
+        Parameters
+        ----------
+        pandas.DataFrame
+            A DataFrame containing all original rows
+    
+        Returns
+        -------
+        pandas.DataFrame
+            A DataFrame containing non-empty rows.
+    """
+    df_drop_rows = df.dropna(axis=0, how='all')
+    return df_drop_rows
+
+def df_formater(df):
+    df.insert(loc=0, column='State', value=" ")
+    check_list = ['Total population', 'Estimate', 'Margin of Error']
+    fill_value = None
+    for index, row in df.iterrows():
+        value = row['Label (Grouping)'].lstrip()
+        # print(value)
+        if value not in check_list:
+            fill_value = value
+            # print(fill_value)
+            df.at[index, 'State'] = fill_value
+        elif value in check_list:
+            fill_value2 = fill_value
+            df.at[index, 'State'] = fill_value2          
+        
+    return df
